0d83d38060cd4b8976162ba5dc28f5eb5ba61cae
ratuamelia18 <ratuamelia225@gmail.com>
2025-03-20 20:11:49 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+        Mobil mobil1 = new Mobil("B 2359 ALO", "Toyota Raize", 500000, true);
+        Mobil mobil2 = new Mobil("B 7899 DDD", "Honda Jazz", 600000, true);
+        Mobil mobil3 = new Mobil("D 4453 EFI", "Mitsubishi Pajero", 800000, true);
+
+        System.out.println(" == Daftar Mobil ==");
+        mobil1.tampilkanInfo();
+        System.out.println();
+        mobil2.tampilkanInfo();
+        System.out.println();
+        mobil3.tampilkanInfo();
+        System.out.println("---------------------");
+
+        Pelanggan pelanggan1 = new Pelanggan("Andi Nicholas", "32100123468678901", "08123412357");
+
+        Sewa sewa1 = new Sewa(pelanggan1, mobil1, 7);
+        sewa1.tampilkanStruk();
+
+        System.out.println("\n == Status Mobil Setelah Transaksi ==");
+        mobil1.tampilkanInfo();
+        System.out.println();
+        mobil2.tampilkanInfo();
+        System.out.println();
+        mobil3.tampilkanInfo();
+    }
+}
